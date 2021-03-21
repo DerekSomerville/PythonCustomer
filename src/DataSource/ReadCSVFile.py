71f63b1d15c4c504329de067e460fdf22bd1e4bf
sandydavidson95 <69345986+sandydavidson95@users.noreply.github.com>
2021-03-21 20:18:48 +0000

    Changed the Smoothie flavours, added prices and changed Smoothie_Menu to a class

--- src/DataSource/ReadCSVFile.py
+++ src/DataSource/ReadCSVFile.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 class ReadCSVFile :
 
-    filePathPrefix = str(Path("../resource/").resolve())
+    filePathPrefix = str(Path("../../resource/").resolve())
     def getFileData(self, directory,  fileName):
         fileData = []
         with open(self.filePathPrefix + "/" + directory + fileName,'rt')as dataFile:
--- src/Display/ReadSmoothieFile.py
+++ src/Display/ReadSmoothieFile.py
@@ -2,8 +2,14 @@ import csv
 from src.DataSource.ReadCSVFile import ReadCSVFile
 from src.DataSource.DataSourceConstants import *
 
-def Smoothie_Menu():
-    readCSVFile = ReadCSVFile()
-    smoothie_Menu = readCSVFile.getFileData(ENTITIES_FOLDER, "Smoothies" + ".csv")
-    return smoothie_Menu
+class Menu:
+    def Smoothie_Menu(self):
+        readCSVFile = ReadCSVFile()
+        smoothie_Menu = readCSVFile.getFileData(ENTITIES_FOLDER, "Smoothies" + ".csv")
+        return smoothie_Menu
 
+def main():
+    Smoothie_Menu()
+main()
+
+print(Smoothie_Menu())
