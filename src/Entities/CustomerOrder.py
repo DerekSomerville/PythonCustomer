aab64efac49fbcbf8b7f356ac9f17f110aaa0df7
Greg <2608073R@student.gla.ac.uk>
2021-03-23 14:25:41 +0000

    Create My Order/ Add Items with CSV Implemented camelCase

--- src/Entities/CustomerOrder.py
+++ src/Entities/CustomerOrder.py
@@ -5,20 +5,20 @@ class CustomerOrder:
         self.orderedSmoothies = orderedItems
 
     def placeOrder(self):
-        order_number = int(input())
+        orderNumber = int(input())
         smoothieMenu = Menu.SmoothieMenu(self)
         while True:
             for item in smoothieMenu:
-                if item[0][0] == str(order_number):
+                if item[0][0] == str(orderNumber):
                     self.orderedSmoothies.append(item[1])
                     elem = self.orderedSmoothies.pop()
                     self.orderedSmoothies.append(elem)
                     print("You have added", elem)
             print("Do you want to order anything else, 1 for yes, 2 for no")
-            continue_ordering = int(input())
-            if continue_ordering == 1:
+            continueOrdering = int(input())
+            if continueOrdering == 1:
                 print("Pick another number to order")
                 return self.placeOrder()
-            elif continue_ordering >= 2 or continue_ordering < 1:
+            elif continueOrdering >= 2 or continueOrdering < 1:
                 return "You have ordered:\n " + str(self.orderedSmoothies).replace("[", "").replace("]", ""). \
                     replace("(", "").replace(")", "").replace(",", "\n").replace("'", "") + " Smoothie"
